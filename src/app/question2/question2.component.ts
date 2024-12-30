f8fea3185747d786fa913f6a8f7dda4a3be14f6f
Michael Scuteri <scuteri.michael@gmail.com>
2024-12-29 22:40:16 -0500

    better random

--- src/app/question2/question2.component.ts
+++ src/app/question2/question2.component.ts
@@ -1,12 +1,6 @@
-import { Component, input } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { BehaviorSubject, delay, interval, map, max, Observable } from 'rxjs';
-import { Question1Form } from './question2.model';
+import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BehaviorSubject, interval, Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -17,8 +11,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './question2.component.scss',
 })
 export class Question2Component {
-  public readonly fizzDivisor: Observable<number>;
-  public readonly buzzDivisor: Observable<number>;
+  private readonly _fizzDivisor: BehaviorSubject<number> = new BehaviorSubject(
+    1
+  );
+  public readonly fizzDivisor: Observable<number> =
+    this._fizzDivisor.asObservable();
+  private readonly _buzzDivisor: BehaviorSubject<number> = new BehaviorSubject(
+    1
+  );
+  public readonly buzzDivisor: Observable<number> =
+    this._buzzDivisor.asObservable();
   public readonly fizzBuzzResult: Observable<string>;
 
   public readonly inputNumber: FormControl<number> = new FormControl<number>(
@@ -29,18 +31,13 @@ export class Question2Component {
   );
 
   constructor() {
-    this.fizzDivisor = interval(2000).pipe(
-      map(() => {
-        return Math.floor(this.generateRandomNumber(2, 10));
-      })
-    );
+    interval(2800).subscribe(() => {
+      this._fizzDivisor.next(this.generateRandomNumber(2, 10));
+    });
 
-    this.buzzDivisor = interval(2000).pipe(
-      delay(1000),
-      map(() => {
-        return Math.floor(this.generateRandomNumber(2, 10));
-      })
-    );
+    interval(5100).subscribe(() => {
+      this._buzzDivisor.next(this.generateRandomNumber(2, 10));
+    });
 
     // TODO: Redifine this.fizzBuzzResult
     this.fizzBuzzResult = new BehaviorSubject(
@@ -50,9 +47,5 @@ export class Question2Component {
 
   generateRandomNumber(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  start() {
-    console.log('form submitted');
   }
 }
